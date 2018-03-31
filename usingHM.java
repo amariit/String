9427583a3624698a666807318354f03da8dcc04a
Amar Kumar <amaryadav4uall@gmail.com>
2018-04-01 00:35:36 +0530

    Update usingHM.java

--- usingHM.java
+++ usingHM.java
@@ -1,32 +1,24 @@
-package odessa;
-
 import java.util.HashMap;
-//import java.util.Map;
 import java.util.Scanner;
-//import java.util.Set;
 
-public class usingHM {
+public class CharOccurance {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the String : ");
 		String s = sc.next();
 		HashMap<Character,Integer> hm = new HashMap<>();
+		//Inserting the character into HashMap
 		for(char c : s.toCharArray()) {
+			//if character is found then only increase the character value of HashMap
 			if(hm.containsKey(c)) {
-				//int val = hm.get(c);
 				hm.put(c, hm.get(c) + 1);
-				//hm.put(c, val+1);
-			}
+				}
+			//Inserting the character value into the HashMap
 			else {
 				hm.put(c, 1);
 			}
 		}
-		//Set<Map.Entry<Character, Integer>> numSet = hm.entrySet();
-		//for(Map.Entry<Character, Integer> m : numSet){
-			//System.out.println("Char- " + m.getKey() + " Count " + m.getValue());
-		//}
 		for ( Character key : hm.keySet()){
 			if(hm.get(key)>1)
 				System.out.println("Character : '"+key+"' Count :"+hm.get(key));
